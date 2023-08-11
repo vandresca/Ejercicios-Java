9e7a1454e4d61a1d5b4dfaf82a8a4bc8eb5d5f55
Víctor Andrés <102297884+vandresca@users.noreply.github.com>
2023-08-11 18:40:23 +0200

    Plantilla de ejercicio

---
+++ Reto #/src/main/java/com/vandresca/Main.java
@@ -0,0 +1,8 @@
+package com.vandresca;
+
+
+public class Main{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Reto #/src/test/java/TestXX.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestXX {
+    
+   
+}
