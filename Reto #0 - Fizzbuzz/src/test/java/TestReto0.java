fd40c39dd50cc0a6b55cbb54f1df1ca3d4dc616f
Víctor Andrés <102297884+vandresca@users.noreply.github.com>
2023-08-11 15:59:46 +0200

    Mejoras maven

--- Reto #0 - Fizzbuzz/src/test/java/TestReto0.java
+++ Reto #0 - Fizzbuzz/src/test/java/TestReto0.java
@@ -1,6 +1,6 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 import com.vandresca.BuzzRule;
 import com.vandresca.FizzBuzz;
@@ -12,7 +12,7 @@ public class TestReto0 {
     FizzBuzz fizzbuzz;
 
 
-    @BeforeAll
+    @Before
     public void testBefore(){
         IRule[] rules = {new FizzRule(), new BuzzRule()};
         fizzbuzz = new FizzBuzz(rules);
@@ -20,28 +20,28 @@ public class TestReto0 {
 
     @Test
     public void checkBuzz(){
-        Assertions.assertEquals(fizzbuzz.getValueOfNumber(3), "buzz");
-        Assertions.assertNotEquals(fizzbuzz.getValueOfNumber(4), "buzz");
-        Assertions.assertNotEquals(fizzbuzz.getValueOfNumber(20),"buzz");
-        Assertions.assertEquals(fizzbuzz.getValueOfNumber(9), "buzz");
-        Assertions.assertNotEquals(fizzbuzz.getValueOfNumber(15), "buzz");
+        Assert.assertEquals(fizzbuzz.getValueOfNumber(3), "buzz");
+        Assert.assertNotEquals(fizzbuzz.getValueOfNumber(4), "buzz");
+        Assert.assertNotEquals(fizzbuzz.getValueOfNumber(20),"buzz");
+        Assert.assertEquals(fizzbuzz.getValueOfNumber(9), "buzz");
+        Assert.assertNotEquals(fizzbuzz.getValueOfNumber(15), "buzz");
     }
 
     @Test
     public void checkFizz(){
-        Assertions.assertEquals(fizzbuzz.getValueOfNumber(5), "fizz");
-        Assertions.assertNotEquals(fizzbuzz.getValueOfNumber(7), "fizz");
-        Assertions.assertNotEquals(fizzbuzz.getValueOfNumber(3), "fizz");
-        Assertions.assertEquals(fizzbuzz.getValueOfNumber(25), "fizz");
-        Assertions.assertNotEquals(fizzbuzz.getValueOfNumber(15), "fizz");
+        Assert.assertEquals(fizzbuzz.getValueOfNumber(5), "fizz");
+        Assert.assertNotEquals(fizzbuzz.getValueOfNumber(7), "fizz");
+        Assert.assertNotEquals(fizzbuzz.getValueOfNumber(3), "fizz");
+        Assert.assertEquals(fizzbuzz.getValueOfNumber(25), "fizz");
+        Assert.assertNotEquals(fizzbuzz.getValueOfNumber(15), "fizz");
     }
 
     @Test
     public void checkFizzBuzz(){
-        Assertions.assertEquals(fizzbuzz.getValueOfNumber(15), "fizzbuzz");
-        Assertions.assertNotEquals(fizzbuzz.getValueOfNumber(7), "fizzbuzz");
-        Assertions.assertNotEquals(fizzbuzz.getValueOfNumber(3), "fizzbuzz");
-        Assertions.assertEquals(fizzbuzz.getValueOfNumber(30), "fizzbuzz");
-        Assertions.assertNotEquals(fizzbuzz.getValueOfNumber(5), "fizzbuzz");
+        Assert.assertEquals(fizzbuzz.getValueOfNumber(15), "fizzbuzz");
+        Assert.assertNotEquals(fizzbuzz.getValueOfNumber(7), "fizzbuzz");
+        Assert.assertNotEquals(fizzbuzz.getValueOfNumber(3), "fizzbuzz");
+        Assert.assertEquals(fizzbuzz.getValueOfNumber(30), "fizzbuzz");
+        Assert.assertNotEquals(fizzbuzz.getValueOfNumber(5), "fizzbuzz");
     }
 }
